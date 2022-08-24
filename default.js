844c3f23b18ae2eee1ffb65eeb3b16948424fbb2
melvinonah <onahmelvin@gmail.com>
2022-08-24 13:36:49 +0100

    Just a major update to the site

---
+++ default.js
@@ -0,0 +1,55 @@
+
+
+
+
+
+// const cart = document.querySelector('.nav__cart');
+// const itemAmount = document.querySelectorAll('.item--amount');
+// const cartSection = document.querySelector('.cart__section');
+// const addCartBtn = document.querySelector('.add--cart');
+// const cartItems = document.querySelector('.cart--items');
+// const deleteItem = document.querySelectorAll('.item--delete');
+// const hamburgerMenu  = document.querySelector('.hamburger--menu');
+// const mobileMenu = document.querySelector('.mobile-nav');
+// const mobileMenuClose = document.querySelector('.mobile-nav-close');
+
+// cart.addEventListener('click', function(e){
+//     console.log(e.target);
+//     cartSection.classList.toggle('hidden');
+// })
+
+// let data = 1;
+
+// itemAmount.textContent = data;
+
+// function decrement(){
+//     while(data >= 1){
+//     data--;
+//     itemAmount.textContent = data;
+//     }
+    
+// }
+
+// function increment(){
+//     data++;
+//     itemAmount.textContent = data;
+//     console.log(itemAmount.textContent);
+
+// }
+
+// function reset(){
+//     data = 1;
+// }
+
+// hamburgerMenu.addEventListener('click', function(){
+//     mobileMenu.classList.remove('hidden');
+//     body.classList.add('fixed');
+//     // body.classList.add('overlay');
+
+// })
+
+// mobileMenuClose.addEventListener('click', function(){
+//     mobileMenu.classList.add('hidden');
+//     body.classList.remove('fixed');
+// })
+
--- script.js
+++ script.js
@@ -1,47 +1,78 @@
 //User Data
 
-const user1 = {
-    name: 'Melvin Onah',
-    password: 'asdf1234',
-    cart: [
-        {
-            name: 'Limited Fall Edition Sneakers',
-            price: 125,
-            quantity: 1
-        },
-        {
-            name: 'Limited Fall Edition Sneakers',
-            price: 125,
-            quantity: 2
-        },
-        {
-            name: 'Limited Fall Edition Sneakers',
-            price: 125,
-            quantity: 2
-        },
-        {
-            name: 'Limited Fall Edition Sneakers',
-            price: 125,
-            quantity: 2
-        }
-    ]
+const admin = {
+    name: 'Melvin',
+    password: 1234,
 }
 
+const  products = [
+    {
+        name: 'Fall Limited Edition Sneakers',
+        id: '109345823',
+
+        ShortDescription: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero vero, impedit molestias quod sint dolore repudiandae cum asperiores non sed debitis nulla mollitia quasi perferendis blanditiis, error quos maxime eveniet.',
+
+        product_1_ImgUrl: 'images\image-product-1.jpg',
+        product_2_ImgUrl: 'images\image-product-2.jpg',
+        product_3_ImgUrl: 'images\image-product-3.jpg',
+
+
+        price: 250.00,
+        discount: 30, //30 percent discount
+
+        productDescription: {
+            name: 'Product Description',
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod porro fuga unde ipsum quis repellat laborum, hic sunt ab distinctio molestiae ullam voluptates minima nostrum possimus fugiat asperiores explicabo sequi. Esse, eum dicta. Soluta a placeat dolores laudantium quibusdam hic ratione libero fugit officia! Dolor esse reiciendis officiis voluptatum dignissimos corporis doloremque totam inventore et. Molestias fugit quod ducimus tempore!'
+        },
+
+        productSpecification: {
+            name: 'Product Specification',
+            brand: 'Lorem',
+            color: 'orange',
+            manufacturer: 'Lorem Industries',
+            manufacturerSerialNumber: 'ib3fnj3eiue3b'
+        },
+
+        warrantyDetails: {
+            name: 'Warranty',
+            warrantyLength: '3-months Warranty',
+            warrantyType: 'Refund'
+        },
+
+        reviews: [{
+            name: 'Melvin',
+            rating: '4.7',
+            review: 'The product is very high quality and finely made!'
+        },
+    
+        {
+            name: 'John',
+            rating: '4.9',
+            review: 'Highly recommended, very good' 
+        },
+
+        {
+            name: 'Dave',
+            rating: '4.8',
+            review: 'The product was easy to clean and was rugged!' 
+        }
+    ]
+    }
+]
 
 
 const cart = document.querySelector('.nav__cart');
-const itemAmount = document.querySelector('.item--amount');
+const itemAmount = document.querySelectorAll('.item--amount');
 const cartSection = document.querySelector('.cart__section');
 const addCartBtn = document.querySelector('.add--cart');
 const cartItems = document.querySelector('.cart--items');
-const deleteItem = document.querySelector('.item--delete');
+const deleteItem = document.querySelectorAll('.item--delete');
 const hamburgerMenu  = document.querySelector('.hamburger--menu');
 const mobileMenu = document.querySelector('.mobile-nav');
 const mobileMenuClose = document.querySelector('.mobile-nav-close');
-const body = document.querySelector('body');
 const carouselImages = document.querySelector('.product__picture');
 const carouselBtn = document.querySelectorAll('.carousel__button');
-const numberOfImages = carouselImages.querySelectorAll('img').length;
+// const numberOfImages = carouselImages.querySelectorAll('img').length;
 const revealBtn = document.querySelector('.reveal-btn');
 const hiddenContent = document.querySelector('.hidden__content')
 const dropDownIcon = document.querySelector('.dropdown--icon');
@@ -51,23 +82,49 @@ const reviewText = document.querySelector('.textarea');
 const reviewRating = document.querySelector('.reviewInput--rating');
 const submitReview = document.querySelector('.submitReview');
 const reviewsSection = document.querySelector('.reviews');
+const increaseQty = document.querySelectorAll('.add--item');
+const reduceQty = document.querySelectorAll('remove--item');
 
-// dropDownIcon.addEventListener('click', function(){
-//     dropDownIcon.classList.toggle('rotate');
-//     // dropDownText.classList.toggle('hidden');
-// })
+let data = 2;
+increaseQty.forEach(btn => {
+
+    itemAmount.forEach(num => {
+        num.textContent = data;
+        // console.log(value);
+        num.textContent = data;
+    
+        btn.addEventListener('click', function(){
+            while(data >= 1){
+                data++;
+                num.textContent = data;
+                }
+        })
+    })
+
+   
+})
 
 
 
-// revealBtn.addEventListener('click', function(){
-//     if(hiddenContent.classList.contains('hidden')){
-//         hiddenContent.classList.remove('hidden');
-//     } else {
-//         hiddenContent.classList.add('hidden');
+
+// function decrement(){
+//     while(data >= 1){
+//     data--;
+//     itemAmount.textContent = data;
 //     }
-// })
+// }
 
-addtoCart(user1.cart);
+function increment(){
+    data++;
+    itemAmount.textContent = data;
+    console.log(itemAmount.textContent);
+
+}
+
+function reset(){
+    data = 1;
+}
+
 
 let imageIndex = 1;
 let translateX = 0;
@@ -121,38 +178,16 @@ cart.addEventListener('click', function(e){
     console.log(e.target);
     cartSection.classList.toggle('hidden');
 })
-console.log(itemAmount.textContent);
+// console.log(itemAmount.textContent);
 
 
-document.addEventListener('click', function(e){
-        if(cartSection.classList.contains('hidden')){
-            cartSection.classList.add('hidden'); 
-        }           
-})
+// document.addEventListener('click', function(e){
+//         if(cartSection.classList.contains('hidden')){
+//             cartSection.classList.add('hidden'); 
+//         }           
+// })
 
 
-let data = 1;
-
-itemAmount.textContent = data;
-
-function decrement(){
-    while(data >= 1){
-    data--;
-    itemAmount.textContent = data;
-    }
-    
-}
-
-function increment(){
-    data++;
-    itemAmount.textContent = data;
-    console.log(itemAmount.textContent);
-
-}
-
-function reset(){
-    data = 1;
-}
 
 function addtoCart(cart){
     cart.forEach(item => {
@@ -166,7 +201,6 @@ function addtoCart(cart){
   </div>`;
 
   cartItems.insertAdjacentHTML('beforeend', markup);
-//   deleteItem.classList.remove('hidden');
     })
 }
 
@@ -197,26 +231,26 @@ addCartBtn.addEventListener('click', function(){
 
 })
 
-submitReview.addEventListener('click', function(e){
-    e.preventDefault();
-    console.log(typeof reviewRating.value);
+// submitReview.addEventListener('click', function(e){
+//     e.preventDefault();
+//     console.log(typeof reviewRating.value);
     
-    if(typeof +reviewRating.value !== 'number'){
-        alert('Please enter a valid Rating Value');
-    }
+//     if(typeof +reviewRating.value !== 'number'){
+//         alert('Please enter a valid Rating Value');
+//     }
 
-    const reviewMarkup = `<div class="review">
-      <div class="review__user--info">
-        <img src="images/image-avatar.png" class="nav__profile" alt="">
-        <h4 class="review__user--name">${reviewInputName.value}</h4>
-        <p class="review__rating">${reviewRating.value} out of 5</p>
-      </div>
-      <div class="review__text">
-        <p>${reviewText.value}</p>
-      </div>`
+//     const reviewMarkup = `<div class="review">
+//       <div class="review__user--info">
+//         <img src="images/image-avatar.png" class="nav__profile" alt="">
+//         <h4 class="review__user--name">${reviewInputName.value}</h4>
+//         <p class="review__rating">${reviewRating.value} out of 5</p>
+//       </div>
+//       <div class="review__text">
+//         <p>${reviewText.value}</p>
+//       </div>`
 
-      reviewsSection.insertAdjacentHTML('beforeend', reviewMarkup);
-})
+//       reviewsSection.insertAdjacentHTML('beforeend', reviewMarkup);
+// })
 
 
 // // deleteItem.addEventListener('click', function(e){
@@ -261,4 +295,14 @@ mobileMenuClose.addEventListener('click', function(){
 //     moveChair(){};    
 // }
 
-localStorage.setItem('currentUser', user1);
+deleteItem.forEach(item => {
+    item.addEventListener('click', function(e){
+        console.log(e);
+    })
+})
+
+// const redirect = setTimeout(function(){
+//     location.href = 'collections.html';
+// }, 6000)
+
+
